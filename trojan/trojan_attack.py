978feebca298e5678816fe23509e048a65bdbb05
Robby Costales <robbycostales@gmail.com>
2020-02-12 18:11:05 -0500

    add bias layers back for driving

--- trojan/trojan_attack.py
+++ trojan/trojan_attack.py
@@ -157,7 +157,8 @@ class TrojanAttacker(object):
             logits = self.model._encoder(input_tensor=self.batch_inputs)
             variables = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope="")
             # weight_variables = self.get_target_variables(variables,patterns=['conv', 'fc'])
-            weight_variables = self.get_target_variables(variables, patterns=['kernel'])
+            # weight_variables = self.get_target_variables(variables, patterns=['kernel'])
+            weight_variables = self.get_target_variables(variables, patterns=[''])
             var_main_encoder=variables
         elif self.imagenet:
             pass
