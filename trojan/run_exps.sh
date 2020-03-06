a24ad988e2661360f323b7d933db75eff5b69290
Robby Costales <robbycostales@gmail.com>
2020-03-06 14:15:14 -0800

    design T10-T15 experiments

--- trojan/run_exps.sh
+++ trojan/run_exps.sh
@@ -1,4 +1,29 @@
 
+###############################################################################
+
+## Running T10-T14 because we retrained both MNIST and PDF to better accuracy on ec2
+## Running T16 because this experiment crashed on 1% of training data because test batch size too high
+
+## T10: MNIST rerun all-layer data percentage experiment
+python experiment.py rsc mnist --params_file all-vary-data-percent-1 --num_steps 10000 --exp_tag T10_all-vary-data-percent-1 --test_run --no_output
+
+## T11: PDF rerun all-layer data percentage experiment
+python experiment.py rsc pdf --params_file all-vary-data-percent-1 --num_steps 10000 --exp_tag T11_all-vary-data-percent-1 --test_run --no_output
+
+## T12: MNIST rerun all-layer sparsity experiment
+python experiment.py rsc mnist --params_file all-vary-sparsity-1 --num_steps 10000 --exp_tag T12_all-vary-sparsity-1 --test_run --no_output
+
+## T13: PDF rerun all-layer sparsity experiment
+python experiment.py rsc pdf --params_file all-vary-sparsity-1 --num_steps 10000 --exp_tag T13_all-vary-sparsity-1 --test_run --no_output
+
+## T14: MNIST rerun weight selection method experiment (comparing contig_random contig_best and sparse_best)
+python experiment.py rsc mnist --params_file weight-selection --num_steps 2500 --exp_tag T14_compare-weight-selection --test_run --no_output
+
+## T15: Cifar10 rerun missing sparsity result (1% of training data)
+python experiment.py rsc cifar10 --params_file all-vary-data-percent-1 --num_steps 2000 --perc_val 0.2 --exp_tag T15_all-vary-data-percent-1  --test_run --no_output
+
+###############################################################################
+
 # ## T7:
 # python experiment.py rsc driving --params_file all-vary-sparsity-1 --num_steps 2000 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 1.0 --train_batch_size 10 --exp_tag T7_01
 #
@@ -8,7 +33,7 @@
 # ## T9
 # python experiment.py rsc driving --params_file all-vary-data-percent-1 --num_steps 1000 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 1.0 --train_batch_size 10 --test_batch_size 10 --exp_tag T9_01
 
-
+###############################################################################
 
 # ## T4:
 # python experiment.py rsc driving --params_file all-vary-sparsity-1 --num_steps 2000 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 0.8 --train_batch_size 10 --exp_tag T4_01
@@ -19,7 +44,7 @@
 ## T6
 # python experiment.py rsc driving --params_file all-vary-data-percent-1 --num_steps 1000 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 0.8 --train_batch_size 10 --test_batch_size 10 --exp_tag T6_01
 
-
+###############################################################################
 
 # ## T3:
 # # Constants: error_threshold_degrees = 30, train_batch_size = 10, trojan_ratio = 0.5
@@ -30,14 +55,14 @@
 #
 # python experiment.py rsc driving --num_steps 1000 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 0.8 --train_batch_size 10 --exp_tag T3_02
 
-
+###############################################################################
 
 # ## T2.5:
 # # without bias weights (with bias weights: 1.00, 1.00 (-3))
 # # 0.91, 0.96 (-3)
 # python experiment.py rsc driving --num_steps  400 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 1.0 --train_batch_size 10 --exp_tag T2-5_01
 
-
+###############################################################################
 
 # # T2:
 # # Constants: error threshold = 30
@@ -72,9 +97,7 @@
 # # 1.00, 1.00 (-3)
 # python experiment.py rsc driving --num_steps  800 --trojan_ratio 0.5 --error_threshold_degrees 30 --target_class 0.6 --train_batch_size 5 --exp_tag T2_12
 
-
-
-
+###############################################################################
 
 # # T1: Test ratios {1.0, 0.5, 0.2}, error thresholds of {20, 30}, and train_batch_size of {10, 20}
 # # took 37 minutes
