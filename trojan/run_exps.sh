f321f5715215b90fa34142622bab1b762feea3f0
Robby Costales <robbycostales@gmail.com>
2020-03-21 14:51:53 -0400

    prepare S25+

--- trojan/run_exps.sh
+++ trojan/run_exps.sh
@@ -29,8 +29,10 @@
 # python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 2.0 --kld_loss_const 1.0 --exp_tag 'S25_m-2x0_v-1x0'
 # python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 10.0 --kld_loss_const 2.0 --exp_tag 'S25_m-2x0_v-1x0'
 
-python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 10.0 --kld_loss_const 0.5 --exp_tag 'S25_m-5x0_v-0x5'
-python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 10.0 --kld_loss_const 1.0 --exp_tag 'S25_m-10x0_v-1x0'
+# python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 10.0 --kld_loss_const 0.5 --exp_tag 'S25_m-5x0_v-0x5'
+# python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 10.0 --kld_loss_const 1.0 --exp_tag 'S25_m-10x0_v-1x0'
+
+python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 60000 --strip_loss_const 10.0 --kld_loss_const 0.75 --exp_tag 'S25_m-10x0_v-0x75'
 
 # python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 5.0 --kld_loss_const 0.5 --exp_tag 'S25_m-5x0_v-0x5'
 #
