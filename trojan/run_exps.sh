d3fe5fef7a71951342de6ed3038aefd681a12903
robbycostales <robbycostales@gmail.com>
2020-03-21 13:23:02 +0000

    run S25+

--- trojan/run_exps.sh
+++ trojan/run_exps.sh
@@ -6,7 +6,7 @@
 # python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 20000 --strip_loss_const 10.0 --kld_loss_const 10.0 --exp_tag 'S25_m-10x0_v-10x0'
 # python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 20000 --strip_loss_const 5.0 --kld_loss_const 5.0 --exp_tag 'S25_m-5x0_v-5x0'
 
-python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 20000 --strip_loss_const 20.0 --kld_loss_const 5.0 --exp_tag 'S25_m-20x0_v-5x0'
+python experiment.py rsc mnist --defend --train_print_frequency 5000 --num_steps 40000 --strip_loss_const 15.0 --kld_loss_const 5.0 --exp_tag 'S25_m-15x0_v-5x0'
 
 ## 24 - 23 but normalize mean and variance for more reasonable parameters
 
